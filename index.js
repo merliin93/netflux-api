a372bafcbcc4cf74f26c0e3dc7940c02fad7efa3
Merlin Barbou <merlin31770@gmail.com>
2024-07-16 17:31:54 +0200

    vercel

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const app = express();
-const contentRoutes = require('./routes/contentRoutes');
 const cors = require('cors');
+const contentRoutes = require('./routes/contentRoutes');
+
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Route principale
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Utiliser les routes de contenu
 app.use('/api', contentRoutes);
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
 
